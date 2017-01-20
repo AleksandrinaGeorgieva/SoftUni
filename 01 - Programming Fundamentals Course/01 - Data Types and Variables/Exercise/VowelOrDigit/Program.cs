5d2b5baf8c9e83e3b19c67fb0a4ff777db37b4a3
AleksandrinaGeorgieva <alex.dm1@gmail.com>
2017-01-20 17:06:40 +0200

    vowels or digit - finished

---
+++ 01 - Programming Fundamentals Course/01 - Data Types and Variables/Exercise/VowelOrDigit/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VowelOrDigit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char symbol = char.Parse(Console.ReadLine());
+            char[] vowels = {'a', 'o', 'e', 'i', 'u', 'y'};
+            int n;
+            bool isNumeric = int.TryParse(symbol.ToString(), out n);
+            if (isNumeric)
+            {
+                Console.WriteLine("digit");
+            }
+            else if (vowels.Contains(symbol))
+            {
+                Console.WriteLine("vowel");
+            }
+            else
+            {
+                Console.WriteLine("other");
+            }
+        }
+    }
+}
---
+++ 01 - Programming Fundamentals Course/01 - Data Types and Variables/Exercise/VowelOrDigit/Readme.md
@@ -0,0 +1,42 @@
+<h1>13.	Vowel or Digit</h1>
+</br>
+Create a program to check if given symbol is digit, vowel or any other symbol.
+</br>
+
+<h4>Examples</h4>
+</br>
+<table>
+	<tr>
+		<th>Input</th>
+		<th>Output</th>
+	</tr>
+	<tr>
+		<td>
+			a
+		</td>
+		<td>
+			vowel
+		</td>
+	</tr>
+	<tr>
+		<td>
+			9
+		</td>
+		<td>
+			digit
+		</td>
+	</tr>
+	<tr>
+		<td>
+			g
+		</td>
+		<td>
+			other
+		</td>
+	</tr>
+</table>
+
+</br>
+<h4>Hints</h4>
+</br>
+•	Use Math.Sqrt() to calculate square root for calculating the diagonal (c2 = a2 + b2). See http://www.mathopenref.com/rectanglediagonals.html.
