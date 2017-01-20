82f903e966704b0cb8e47af618efe051e4fbba17
AleksandrinaGeorgieva <alex.dm1@gmail.com>
2017-01-20 10:58:46 +0200

    centuries to minutes - finished

--- 01 - Programming Fundamentals Course/01 - Data Types and Variables/Lab/CenturiesToMinutes/Program.cs
+++ 01 - Programming Fundamentals Course/01 - Data Types and Variables/Lab/CenturiesToMinutes/Program.cs
@@ -6,10 +6,30 @@ using System.Threading.Tasks;
 
 namespace CenturiesToMinutes
 {
+    static class Constants
+    {
+        public const int yearsInCenturies = 100;
+        public const double daysInYear = 365.2422;
+        public const short hoursInDay = 24;
+        public const short minutesInHour = 60;
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            int centuries = int.Parse(Console.ReadLine());
+            int years = centuries * Constants.yearsInCenturies;
+            int days = (int) (years * Constants.daysInYear);
+            long hours = days * Constants.hoursInDay;
+            long minutes = hours * Constants.minutesInHour;
+
+            Console.WriteLine("{0} centuries = {1} years = {2} days = {3} hours = {4} minutes", 
+                centuries,
+                years, 
+                days, 
+                hours,
+                minutes);
+
         }
     }
 }
