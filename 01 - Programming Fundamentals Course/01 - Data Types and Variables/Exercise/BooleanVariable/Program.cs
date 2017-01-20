5f453c4411f554b3becce1a9ba3f5df6a52d124f
AleksandrinaGeorgieva <alex.dm1@gmail.com>
2017-01-20 13:43:00 +0200

    Boolean Variable - finished

---
+++ 01 - Programming Fundamentals Course/01 - Data Types and Variables/Exercise/BooleanVariable/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BooleanVariable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string answer = Console.ReadLine();
+            Console.WriteLine(Convert.ToBoolean(answer) ? "Yes" : "No");
+        }
+    }
+}
