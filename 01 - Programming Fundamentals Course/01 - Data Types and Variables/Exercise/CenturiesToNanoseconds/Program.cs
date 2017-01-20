e523faf4499f76620e1068bdad7ee3ec841425f0
AleksandrinaGeorgieva <alex.dm1@gmail.com>
2017-01-20 14:57:25 +0200

    centuries to nanoseconds - finished

---
+++ 01 - Programming Fundamentals Course/01 - Data Types and Variables/Exercise/CenturiesToNanoseconds/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace CenturiesToNanoseconds
+{
+    static class Constants
+    {
+        public const int yearsInCenturies = 100;
+        public const double daysInYear = 365.2422;
+        public const short hoursInDay = 24;
+        public const short minutesInHour = 60;
+        public const short secondsInMinute = 60;
+        public const int milliSecondsInSecond = 1000;
+        public const int microSecondsInMilliSecond = 1000;
+        public const int nanoSecondsInMicroSecond = 1000;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int centuries = int.Parse(Console.ReadLine());
+            int years = centuries * Constants.yearsInCenturies;
+            int days = (int)(years * Constants.daysInYear);
+            long hours = days * Constants.hoursInDay;
+            long minutes = hours * Constants.minutesInHour;
+            long seconds = minutes * Constants.secondsInMinute;
+            BigInteger milliSeconds = seconds * Constants.milliSecondsInSecond;
+            BigInteger microSeconds = milliSeconds * Constants.microSecondsInMilliSecond;
+            BigInteger nanoSeconds = microSeconds * Constants.nanoSecondsInMicroSecond;
+
+            Console.WriteLine("{0} centuries = {1} years = {2} days = {3} hours = {4} minutes = {5} seconds = {6} milliseconds = {7} microseconds = {8} nanoseconds",
+                centuries,
+                years,
+                days,
+                hours,
+                minutes,
+                seconds,
+                milliSeconds,
+                microSeconds,
+                nanoSeconds);
+
+        }
+    }
+}
---
+++ 01 - Programming Fundamentals Course/01 - Data Types and Variables/Exercise/CenturiesToNanoseconds/Readme.md
@@ -0,0 +1,30 @@
+<h1>10.	Centuries to Nanoseconds</h1>
+</br>
+Write program to enter an integer number of centuries and convert it to years, days, hours, minutes, seconds, milliseconds, microseconds, nanoseconds.
+</br>
+<h4>Examples</h4>
+</br>
+<table>
+	<tr>
+		<th>Input</th>
+		<th>Output</th>
+	</tr>
+	<tr>
+		<td>
+			1
+		</td>
+		<td>1 centuries = 100 years = 36524 days = 876576 hours = 52594560 minutes = 3155673600 seconds = 3155673600000 milliseconds = 3155673600000000 microseconds = 3155673600000000000 nanoseconds
+		</td>
+	</tr>
+	<tr>
+		<td>
+			5
+		</td>
+		<td>5 centuries = 500 years = 182621 days = 4382904 hours = 262974240 minutes = 15778454400 seconds = 15778454400000 milliseconds = 15778454400000000 microseconds = 15778454400000000000 nanoseconds
+		</td>
+	</tr>
+</table>
+
+</br>
+<h4>Hints</h4>
+</br>
