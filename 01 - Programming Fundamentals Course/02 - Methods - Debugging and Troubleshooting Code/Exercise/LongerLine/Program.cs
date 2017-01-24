8d22bef21e3fddb706d89f31c74bffa498bc4052
AleksandrinaGeorgieva <alex.dm1@gmail.com>
2017-01-24 19:55:39 +0200

    LongerLine

---
+++ 01 - Programming Fundamentals Course/02 - Methods - Debugging and Troubleshooting Code/Exercise/LongerLine/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LongerLine
+{
+    class Program
+    {
+        public static double LineWidthParam(double x1, double y1, double x2, double y2)
+        {
+            return Math.Abs(x1 - x2) + Math.Abs(y1 - x2);
+        }
+
+        public static void PrintLine(double x1, double y1, double x2, double y2)
+        {
+            double firstPointDistance = Math.Abs(x1) + Math.Abs(y1);
+            double secondPointDistance = Math.Abs(x2) + Math.Abs(y2);
+            if (firstPointDistance > secondPointDistance)
+            {
+                Console.WriteLine("({0}, {1})({2}, {3})", x2, y2, x1, y1);
+            }
+            else
+            {
+                Console.WriteLine("({0}, {1})({2}, {3})", x1, y1, x2, y2);
+            }
+        }
+
+        public static void PrintLongerLine()
+        {
+            double x1 = double.Parse(Console.ReadLine());
+            double y1 = double.Parse(Console.ReadLine());
+            double x2 = double.Parse(Console.ReadLine());
+            double y2 = double.Parse(Console.ReadLine());
+
+            double x3 = double.Parse(Console.ReadLine());
+            double y3 = double.Parse(Console.ReadLine());
+            double x4 = double.Parse(Console.ReadLine());
+            double y4 = double.Parse(Console.ReadLine());
+
+            double firstLineWidthParam = LineWidthParam(x1, y1, x2, y2);
+            double secondLineWidthParam = LineWidthParam(x3, y3, x4, y4);
+
+            if (secondLineWidthParam > firstLineWidthParam)
+            {
+                PrintLine( x3, y3, x4, y4);
+            }
+            else
+            {
+                PrintLine(x1, y1, x2, y2);
+            }
+
+        }
+
+        static void Main(string[] args)
+        {
+            PrintLongerLine();
+        }
+    }
+}
---
+++ 01 - Programming Fundamentals Course/02 - Methods - Debugging and Troubleshooting Code/Exercise/LongerLine/Readme.md
@@ -0,0 +1,37 @@
+<H2 CLASS="western">Longer Line</H2>
+  
+<P STYLE="margin-top: 0.06in">You are given the coordinates of four
+points in the 2D plane. The first and the second pair of points form
+two different lines. Print the longer line in format &quot;(X1,
+Y1)(X2, Y2)&quot; starting with the point that is closer to the
+center of the coordinate system (0, 0) (You can reuse the method that
+you wrote for the previous problem). If the lines are of equal
+length, print only the first one.</P>
+<H3 CLASS="western">Examples</H3>
+<TABLE WIDTH=230 CELLPADDING=4 CELLSPACING=0>
+	<COL WIDTH=43>
+	<COL WIDTH=170>
+	<TR VALIGN=TOP>
+		<TD WIDTH=43 BGCOLOR="#d9d9d9" STYLE="border: 1px solid #00000a; padding-top: 0.04in; padding-bottom: 0.04in; padding-left: 0.06in; padding-right: 0.06in">
+			<P ALIGN=CENTER><B>Input</B></P>
+		</TD>
+		<TD WIDTH=170 BGCOLOR="#d9d9d9" STYLE="border: 1px solid #00000a; padding-top: 0.04in; padding-bottom: 0.04in; padding-left: 0.06in; padding-right: 0.06in">
+			<P ALIGN=CENTER><B>Output</B></P>
+		</TD>
+	</TR>
+	<TR VALIGN=TOP>
+		<TD WIDTH=43 STYLE="border: 1px solid #00000a; padding-top: 0.04in; padding-bottom: 0.04in; padding-left: 0.06in; padding-right: 0.06in">
+			<P STYLE="margin-bottom: 0in">2</P>
+			<P STYLE="margin-bottom: 0in">4</P>
+			<P STYLE="margin-bottom: 0in">-1</P>
+			<P STYLE="margin-bottom: 0in">2</P>
+			<P STYLE="margin-bottom: 0in">-5</P>
+			<P STYLE="margin-bottom: 0in">-5</P>
+			<P STYLE="margin-bottom: 0in">4</P>
+			<P>-3</P>
+		</TD>
+		<TD WIDTH=170 STYLE="border: 1px solid #00000a; padding-top: 0.04in; padding-bottom: 0.04in; padding-left: 0.06in; padding-right: 0.06in">
+			<P><FONT FACE="Consolas, serif">(4, -3)(-5, -5)</FONT></P>
+		</TD>
+	</TR>
+</TABLE>
