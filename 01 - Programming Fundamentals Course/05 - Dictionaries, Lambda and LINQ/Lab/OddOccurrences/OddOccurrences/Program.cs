259b925dd4d7659e58033db76358255d331a8804
AleksandrinaGeorgieva <alex.dm1@gmail.com>
2017-02-02 12:17:20 +0200

    odd occurrences

---
+++ 01 - Programming Fundamentals Course/05 - Dictionaries, Lambda and LINQ/Lab/OddOccurrences/OddOccurrences/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OddOccurrences
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var words = Console.ReadLine()
+                .Split(' ')
+                .Select(x => x.ToLower())
+                .ToArray();
+            var occurrences = new Dictionary<string, int>();
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (!(occurrences.ContainsKey(words[i])))
+                {
+                    occurrences[words[i]] = 0;
+                }
+                occurrences[words[i]]++;
+            }
+
+            var oddOccurrences = occurrences.Where(x => x.Value % 2 == 1).ToDictionary(x => x.Key, x => x.Value);
+            Console.WriteLine(string.Join(", ", oddOccurrences.Keys));
+        }
+    }
+}
