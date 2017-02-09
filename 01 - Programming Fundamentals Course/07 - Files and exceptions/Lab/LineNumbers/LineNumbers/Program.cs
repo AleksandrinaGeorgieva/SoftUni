d580dab6f78d0ff9df2c92bb62ffc428407ffb0c
AleksandrinaGeorgieva <alex.dm1@gmail.com>
2017-02-09 18:56:23 +0200

    line numbers

---
+++ 01 - Programming Fundamentals Course/07 - Files and exceptions/Lab/LineNumbers/LineNumbers/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace LineNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var fileLines = File.ReadAllLines("Input.txt");
+            var linesToWrite = fileLines
+                .Select((line, index) => $"{index + 1}. {line}")
+                .ToArray();
+            File.WriteAllLines("Output.txt", linesToWrite);
+        }
+    }
+}
