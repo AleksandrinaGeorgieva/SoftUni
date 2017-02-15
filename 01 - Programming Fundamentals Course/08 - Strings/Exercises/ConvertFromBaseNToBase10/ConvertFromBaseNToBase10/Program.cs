4a5e2e7d4335abd6157b90e57adab718bdfe160c
AleksandrinaGeorgieva <alex.dm1@gmail.com>
2017-02-15 10:12:01 +0200

    convert from base-N to base-10

---
+++ 01 - Programming Fundamentals Course/08 - Strings/Exercises/ConvertFromBaseNToBase10/ConvertFromBaseNToBase10/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace ConvertFromBaseNToBase10
+{
+    using System;
+    using System.Linq;
+    using System.Numerics;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine()
+                .Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+            int N = int.Parse(input[0]);
+            var number = input[1];
+            number = new string(number.Reverse().ToArray());
+
+            BigInteger  result = 0;
+
+            for (int i = 0; i < number.Length; i++)
+            {
+                BigInteger cur = new BigInteger(char.GetNumericValue(number[i]));
+                BigInteger b = BigInteger.Pow(N, i);
+                BigInteger tempSum = (BigInteger)(cur * b);
+                result += tempSum;
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
