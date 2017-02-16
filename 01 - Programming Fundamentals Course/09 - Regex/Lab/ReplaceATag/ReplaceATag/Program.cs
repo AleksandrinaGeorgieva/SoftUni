0136d96a94b9cc585f77357022d539deec047092
AleksandrinaGeorgieva <alex.dm1@gmail.com>
2017-02-16 12:52:56 +0200

    replace a tag

---
+++ 01 - Programming Fundamentals Course/09 - Regex/Lab/ReplaceATag/ReplaceATag/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace ReplaceATag
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text.RegularExpressions;
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            string pattern = @"<a(.*href=(?:'|"").*?(?:'|""))>(.*?)<\/a>";
+            Regex regex = new Regex(pattern);
+
+            var resultList = new List<string>();
+            var text = Console.ReadLine();
+            while(text != "end")
+           {
+                string replacement = Regex.Replace(text, @"\t|\n|\r", "");
+                var matches = regex.Matches(replacement);
+                replacement = regex.Replace(replacement, "[URL$1]$2[/URL]");
+                resultList.Add(replacement);
+                text = Console.ReadLine();
+            }
+
+            resultList.ForEach(Console.WriteLine);
+        }
+    }
+}
