d1eeed2ab321a020085878622b5dbb93646d5a4d
AleksandrinaGeorgieva <alex.dm1@gmail.com>
2017-03-20 12:42:08 +0200

    phonebook

---
+++ 03 - C# Advanced/02 - Sets and Dictionaries/Phonebook/Phonebook/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Phonebook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var data = new Dictionary<string, string>();
+            var input = Console.ReadLine();
+
+            while (input.ToLower() != "search")
+            {
+                var splitted = input
+                    .Split('-');
+
+                if (!data.ContainsKey(splitted[0]))
+                {
+                    data[splitted[0]] = "";
+                }
+                data[splitted[0]] = splitted[1];
+
+                input = Console.ReadLine();
+            }
+
+            input = Console.ReadLine();
+
+            while(input.ToLower() != "stop")
+            {
+                if (data.ContainsKey(input))
+                {
+                    Console.WriteLine($"{input} -> {data[input]}");
+                }else
+                {
+                    Console.WriteLine($"Contact {input} does not exist.");
+                }
+                input = Console.ReadLine();
+            }            
+        }
+    }
+}
