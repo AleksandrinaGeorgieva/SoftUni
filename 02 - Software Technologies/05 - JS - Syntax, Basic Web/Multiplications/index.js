07deb01dfa5590594142851a4c3ea7c67df51ba2
AleksandrinaGeorgieva <alex.dm1@gmail.com>
2017-03-22 08:23:51 +0200

    product of three numbers

--- 02 - Software Technologies/05 - JS - Syntax, Basic Web/Multiplications/index.js
+++ 02 - Software Technologies/05 - JS - Syntax, Basic Web/Multiplications/index.js
@@ -18,3 +18,22 @@ function MultiplyOrDivideANumber(arr){
         return a / b;
     }
 }
+
+function ProductOfThreeNumbers(arr){
+    arr = arr
+        .map(Number)
+        .filter(x => x < 0);
+
+    let negatives = arr
+        .filter(x => x == 0);
+
+    if(arr.length % 2 == 0
+        || negatives.length
+    ){
+        return "Positive";
+    }else{
+        return "Negative";
+    }
+}
+
+ProductOfThreeNumbers(['2', '3', '-1']);
