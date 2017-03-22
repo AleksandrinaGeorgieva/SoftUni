78dff79d903541f02a7adee5f69fca727a987e6b
AleksandrinaGeorgieva <alex.dm1@gmail.com>
2017-03-22 09:05:55 +0200

    multiple values per key

--- 02 - Software Technologies/05 - JS - Syntax, Basic Web/WorkingWithKeyValuePairs/index.js
+++ 02 - Software Technologies/05 - JS - Syntax, Basic Web/WorkingWithKeyValuePairs/index.js
@@ -13,3 +13,33 @@ function ObjectData(arr){
         console.log("None");
     }
 }
+
+/*
+* You will be given input lines, each holding two
+* elements separated by a space: key and value.
+* You need to store the given values to the given keys.
+* At the last line of the input you will receive a key.
+* You must print all the values corresponding to that key.
+* If there are no such, just print “None”.
+* */
+
+function ObjectDataMultipleValues(arr){
+    let data = {};
+    for(let i = 0; i < arr.length - 1; i++){
+        let splitted = arr[i]
+            .split(' ');
+        if(!data[splitted[0]]){
+            data[splitted[0]] = [];
+        }
+        data[splitted[0]].push(splitted[1]);
+    }
+
+    let last = arr[arr.length - 1];
+    if(data[last]){
+        for(let i = 0; i < data[last].length; i++){
+            console.log(data[last][i]);
+        }
+    }else{
+        console.log("None");
+    }
+}
