e760f1e47a7d125a0650efa28ea28ba9f7238a2e
AleksandrinaGeorgieva <alex.dm1@gmail.com>
2017-04-28 10:55:32 +0300

    find evens or odds

---
+++ 03 - C# Advanced/05 - Functional Programming/FindEvensOrOdds/FindEvensOrOdds/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindEvensOrOdds
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = Console.ReadLine()
+                .Split();
+            var searchType = Console.ReadLine();
+            int min = int.Parse(numbers[0]);
+            int max = int.Parse(numbers[1]);
+
+            Predicate<int> odd = x => x % 2 != 0;
+            Predicate<int> even = x => x % 2 == 0;
+
+            var list = new List<int>();
+            for (int i = min; i <= max; i++)
+            {
+                list.Add(i);
+            }
+
+            var result = new List<int>();
+
+            if(searchType == "odd")
+            {
+                result = list.FindAll(odd);
+            }else
+            {
+                result = list.FindAll(even);
+            }
+
+            result.ForEach(x => Console.Write($"{x} "));
+        }
+    }
+}
