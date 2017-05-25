e2d17b17daf49bcea482003bd6768fdb50deeb6d
AleksandrinaGeorgieva <alex.dm1@gmail.com>
2017-05-25 06:45:57 +0300

    recursive fibonacci fix

--- 03 - C# Advanced/01 - Stacks and Queues/RecursiveFibonacci/RecursiveFibonacci/Program.cs
+++ 03 - C# Advanced/01 - Stacks and Queues/RecursiveFibonacci/RecursiveFibonacci/Program.cs
@@ -1,20 +1,31 @@
 ﻿namespace RecursiveFibonacci
 {
     using System;
+    using System.Collections.Generic;
     class Program
     {
-        private static int getFibonacci(int n)
+        private static Dictionary<long, long> nums = new Dictionary<long, long>();
+
+        private static long getFibonacci(long n)
         {
+            long f;
             if (n <= 2)
             {
                 return 1;
             }
-            return getFibonacci(n - 1) + getFibonacci(n - 2);
+            if (!nums.ContainsKey(n))
+            {
+                nums[n] = getFibonacci(n - 1) + getFibonacci(n - 2);
+            }
+            f = nums[n];
+            
+            return f;
         }
 
         public static void Main(string[] args)
         {
-            int n = int.Parse(Console.ReadLine());
+            long n = long.Parse(Console.ReadLine());
+            
             Console.WriteLine(getFibonacci(n));
         }
     }
