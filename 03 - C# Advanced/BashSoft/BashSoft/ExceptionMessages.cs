b71e376c06ed5a572abf8206b05e05065950d949
AleksandrinaGeorgieva <alex.dm1@gmail.com>
2017-05-27 09:41:56 +0300

    traversing the folder of the project

---
+++ 03 - C# Advanced/BashSoft/BashSoft/ExceptionMessages.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BashSoft
+{
+    public static class ExceptionMessages
+    {
+        public const string ExampleExceptionMessage = "Example message!";
+        public const string UnauthorizedExceptionMessage = "The folder/file you are trying to get access needs a higher level of rights than you currently have.";
+    }
+}
